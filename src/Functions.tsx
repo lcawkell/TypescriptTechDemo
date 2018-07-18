47005a576101de2a553ec889bc3e503f5c0cc2b5
lcawkell <lucas.cawkell@ubc.ca>
2018-07-18 16:55:19 -0700

    Added TypeScript start

--- src/Functions.tsx
+++ src/Functions.tsx
@@ -14,18 +14,13 @@ export function mountMessages(domNode:string, messages:string[]):void {
     $(domNode).html(messages.join(''));
 }
 
-export function loadMessages(messages:IMessage[], authors:IAuthor[], roles:IRole[]):string[] {
+export function loadMessages(messages:IMessage[], authors:IAuthor[]):string[] {
     return messages.map(message => {
 
         let author = findAuthorById(authors,message.authorId);
-        let role = findRoleById(roles, author.roleId);
 
-        return `<li><span class='message'><strong>${author.name} (${role.name})</strong>: ${message.message}</span></li>`
+        return `<li><span class='message'><strong>${author.name}</strong>: ${message.message}</span></li>`
     });
-}
-
-function findRoleById(roles:IRole[], roleId:number){
-    return roles.filter(role => role.id === roleId)[0];
 }
 
 export function addMessage(messages:IMessage[], message:string, id:number, authorId:number) {
